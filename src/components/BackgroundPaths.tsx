957d7b7448257d7ffbd4b89b307b0b3b0fa34707
izabellecarriel18 <izabelle.carriel18@gmail.com>
2025-11-12 21:36:12 -0300

    Updated BackgroundPaths.tsx

--- src/components/BackgroundPaths.tsx
+++ src/components/BackgroundPaths.tsx
@@ -49,7 +49,7 @@ function FloatingPaths({ position }: { position: number }) {
 export default function BackgroundPaths() {
   return (
     <>
-      <div className="fixed inset-0 -z-10 bg-black">
+      <div className="fixed inset-0 -z-10 bg-[#2a2a2a]">
         <FloatingPaths position={1} />
         <FloatingPaths position={-1} />
       </div>
