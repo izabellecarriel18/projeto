2b691bfe0041e777da0fe042f234831d5218e9e3
izabellecarriel18 <izabelle.carriel18@gmail.com>
2025-11-12 21:48:57 -0300

    Updated BackgroundPaths.tsx

--- src/components/BackgroundPaths.tsx
+++ src/components/BackgroundPaths.tsx
@@ -16,7 +16,7 @@ function FloatingPaths({ position }: { position: number }) {
   return (
     <div className="absolute inset-0 pointer-events-none">
       <svg
-        className="w-full h-full text-white"
+        className="w-full h-full text-blue-950"
         viewBox="0 0 696 316"
         fill="none"
       >
