e3e2e5ccc2ee83096cd1560d866405d537aeddbe
izabellecarriel18 <izabelle.carriel18@gmail.com>
2026-01-27 18:54:34 -0300

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -30,7 +30,7 @@ export default function Header({ onNavigate, currentPage }: HeaderProps) {
   };
 
   return (
-    <header className="fixed top-0 left-0 right-0 bg-blue-950/70 backdrop-blur-sm z-50 border-b border-blue-900/50 safe-top">
+    <header className="fixed top-0 left-0 right-0 bg-blue-950/70 backdrop-blur-sm z-50 lg:border-b lg:border-blue-900/50 safe-top">
       <div className="container mx-auto px-4 sm:px-6 py-5 lg:py-4 flex items-center justify-center lg:justify-between">
         <div
           className="cursor-pointer z-[110]"
